17eeb5e233817943312c2e742edd0caf153770ba
mfrancis95 <mikefrancis95@gmail.com>
2016-03-11 09:07:03 -0500

    Add parameter for counting multiple hits from the same IP

--- hit-counter.js
+++ hit-counter.js
@@ -1,11 +1,35 @@
 var hits = new Map();
 
+var hitCount = 0, countSame;
+
+function clearHits() {
+    hits.clear();
+    hitCount = 0;
+}
+
 function middleware(request, response, next) {
-    hits.set(request.ip, new Date());
+    var ip = request.ip;
+    var hit = hits.get(ip);
+    if (!hit) {
+        hit = {count: 1, last: new Date()};
+        hits.set(ip, hit);
+        hitCount++;
+    }
+    else if (countSame) {
+        var now = new Date();
+        if (now.getTime() - hit.last.getTime() > countSame) {
+            hit.count++;
+            hit.last = now;
+            hitCount++;
+        }
+    }
     next();
 }
 
-module.exports = () => middleware;
-module.exports.clearHits = () => hits.clear();
-module.exports.hitCount = () => hits.size;
-module.exports.hits = callback => hits.forEach(callback);
+module.exports = countSameDelay => {
+    countSame = countSameDelay;
+    return middleware;
+};
+module.exports.clearHits = clearHits;
+module.exports.hitCount = () => hitCount;
+module.exports.hits = () => hits.entries();
