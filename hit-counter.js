7cb69e11470365620d88259d12ee78fa4dec0b43
mfrancis95 <mikefrancis95@gmail.com>
2016-03-21 00:00:26 -0400

    Move code into single function

--- hit-counter.js
+++ hit-counter.js
@@ -1,35 +1,31 @@
-var hits = new Map();
-
-var hitCount = 0, countSame;
-
-function clearHits() {
-    hits.clear();
-    hitCount = 0;
-}
-
-function middleware(request, response, next) {
-    var ip = request.ip;
-    var hit = hits.get(ip);
-    if (!hit) {
-        hit = {count: 1, last: new Date()};
-        hits.set(ip, hit);
-        hitCount++;
-    }
-    else if (countSame) {
-        var now = new Date();
-        if (now.getTime() - hit.last.getTime() > countSame) {
-            hit.count++;
-            hit.last = now;
+function hitCounter(countSame) {
+    var hits = new Map();
+    var hitCount = 0;
+    var middleware = (request, response, next) => {
+        var ip = request.ip;
+        var hit = hits.get(ip);
+        if (!hit) {
+            hit = {count: 1, last: new Date()};
+            hits.set(ip, hit);
             hitCount++;
         }
-    }
-    next();
+        else if (countSame) {
+            var now = new Date();
+            if (now.getTime() - hit.last.getTime() > countSame) {
+                hit.count++;
+                hit.last = now;
+                hitCount++;
+            }
+        }
+        next();
+    };
+    middleware.clearHits = () => {
+        hits.clear();
+        hitCount = 0;
+    };
+    middleware.hitCount = () => hitCount;
+    middleware.hits = () => hits.entries();
+    return middleware;
 }
 
-module.exports = countSameDelay => {
-    countSame = countSameDelay;
-    return middleware;
-};
-module.exports.clearHits = clearHits;
-module.exports.hitCount = () => hitCount;
-module.exports.hits = () => hits.entries();
+module.exports = hitCounter;
